5603b144514c9e8c1c79edfc5a8e8fe159ddd0dc
tahmedofficial <tanvirofcuo@gmail.com>
2024-05-08 22:44:12 +0600

    added pagination using query

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ufkobjs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,13 +26,30 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-    const productCollection = client.db('emaJohnDB').collection('products');
+    const database = client.db('emaJohnDB');
+    const productCollection = database.collection('products');
 
-    app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
-        res.send(result);
+    app.get('/products', async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const skip = page * size;
+      const result = await productCollection.find().skip(skip).limit(size).toArray();
+      res.send(result);
+    })
+
+    app.get("/productsCount", async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({ count });
+    })
+
+    app.post("/productByIds", async (req, res) => {
+      const ids = req.body;
+      const objectId = ids.map(id => new ObjectId(id))
+      const query = { _id: { $in: objectId } }
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
@@ -46,10 +63,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res) =>{
-    res.send('john is busy shopping')
+app.get('/', (req, res) => {
+  res.send('Server is running')
 })
 
-app.listen(port, () =>{
-    console.log(`ema john server is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`ema john server is running on port: ${port}`);
 })
